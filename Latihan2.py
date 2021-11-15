1a65fcc5b05ce9cf50d5735474128678236d74a4
Bhagassp <bhagasshaibp@gmail.com>
2021-11-15 22:38:45 +0700

    first commit

---
+++ Latihan2.py
@@ -0,0 +1,13 @@
+print("Latihan 2 Mengurutkan Data")
+print("==========================")
+
+a = int(input("Masukan Bilangan Ke-1: "))
+b = int(input("Masukan Bilangan Ke-2: "))
+c = int(input("Masukan Bilangan Ke-3: "))
+d = int(input("Masukan Bilangan Ke-4: "))
+e = int(input("Masukan Bilangan Ke-5: "))
+f = int(input("Masukan Bilangan Ke-6: "))
+data = [a,b,c,d,e,f]
+data.sort()
+
+print("Urutan Bilangan: ", data)
